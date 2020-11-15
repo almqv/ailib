4b16fa2b3a6d47c573084c85dbaa526083bfe779
E. Almqvist <elalmqvist@gmail.com>
2020-11-15 15:28:42 +0100

    Added layer generation

--- ailib/neuralnetwork.py
+++ ailib/neuralnetwork.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from copy import deepcopy as copy
 
 import ailib.debug as db
 
@@ -13,9 +14,9 @@ class neural_network:
 
         self.debug( f"Created neural network {self}", db.level.success )
 
-    def debug( self, text:str, lvl:str = db.level.info, indent:int = 0, end:str = "\n" ):
+    def debug( self, text:str, level:str = db.level.info, indent:int = 0, end:str = "\n" ):
         if( self.enableDebug ): # Only debug when it is enabled
-            db.debug( text, lvl, indent, end )
+            db.debug( text, level, indent, end )
 
     def generateLayers( self, neuronDimensions:list = [ 1, 1 ], offset:float = -0.25 ):
         # The neuronDimensions are the dimensions of the neurons. Each index is a layer and that
@@ -25,23 +26,29 @@ class neural_network:
 
         try:
             self.debug( f"Generating layers {neuronDimensions}" )
+            layersLen = len(neuronDimensions)
+            layerProp = [None] * (layersLen - 1)
 
             # Generate the weight matrix
-            self.debug( "Generating weight matrix", indent=1 )
-            self.weights = [None] * (len(neuronDimensions) - 1)
+            self.debug( "Generating weight matrix...", indent=1 )
+            self.weights = copy(layerProp)
 
             for index, neuronCount in enumerate(neuronDimensions): # Iterate through each layer and append the weights
                 if( index > 0 ):
                     self.weights[index - 1] = np.random.rand( neuronDimensions[index-1], neuronCount ) + offset
-            self.debug( f"Generated weights: {self.weights}", indent=1 )
+
+            self.debug( f"Generated weights matrix: {self.weights}", db.level.success, indent=1 )
+
 
             # Generate the bias matrix
-            self.debug( "Generating bias matrix", indent=1 )
-            self.bias = [None] * (len(neuronDimensions) - 1)
+            self.debug( "Generating bias matrix...", indent=1 )
+            self.bias = copy(layerProp)
 
             for index, neuronCount in enumerate(neuronDimensions):
                 if(index > 0):
                     self.bias[index - 1] = np.random.rand( 1, neuronCount ) + offset
+
+            self.debug( f"Generated bias matrix: {self.bias}", db.level.success, indent=1 )
 
 
         except:
