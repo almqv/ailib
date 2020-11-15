e0bc1650c4183829697e799dc0ffa9b8bf43a743
E. Almqvist <elalmqvist@gmail.com>
2020-11-15 13:21:24 +0100

    Added layergen function

--- ailib/neuralnetwork.py
+++ ailib/neuralnetwork.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 class neural_network:
-    def __init__( self, layers: list=[[]] ):
+    def __init__( self, layers: list=None ):
         self.layers = layers
         print(self)
+
+    def generateLayers( self, layerDimensions: list=[ (1,1), (1,1) ] ):
+        print("New layers")
