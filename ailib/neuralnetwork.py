6dbd233d978b8298342c7e740f796c8699036688
E. Almqvist <elalmqvist@gmail.com>
2020-11-15 13:41:54 +0100

    Debug wrapper

--- ailib/neuralnetwork.py
+++ ailib/neuralnetwork.py
@@ -1,9 +1,18 @@
 import numpy as np
 
+import ailib.debug as db
+
+
 class neural_network:
-    def __init__( self, layers: list=None ):
+    def __init__( self, enableDebug: bool=True, layers: list=None ):
         self.layers = layers
-        print(self)
+        self.enableDebug = enableDebug
+
+        self.debug( f"Created neural network {self}" )
+
+    def debug( text:str, lvl:str ):
+        if( self.enableDebug ):
+            db.debug( text, lvl )
 
     def generateLayers( self, layerDimensions: list=[ (1,1), (1,1) ] ):
         print("New layers")
