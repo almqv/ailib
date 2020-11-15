d7f4956996c9294ae631446192c702da8268ce57
E. Almqvist <elalmqvist@gmail.com>
2020-11-15 13:15:57 +0100

    Class stuff and gitignore

--- ailib/neuralnetwork.py
+++ ailib/neuralnetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 class neural_network:
-    def __init__(self):
+    def __init__( self, layers: list=[[]] ):
+        self.layers = layers
         print(self)
---
+++ example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# This is a example on how to use this library.
+# It is also a "testing" file.
+
+from ailib import ai
+
+test = ai.neural_network() # Create a new instace of a network
