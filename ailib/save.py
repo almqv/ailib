fd5259e3ec10ce116fed55e5fc772e22a7ea8cc6
Alve Svarén <alve@hotmail.se>
2021-03-31 17:04:41 +0200

    Fixed style according to pep8

--- ailib/save.py
+++ ailib/save.py
@@ -1,12 +1,14 @@
 import pickle
 
-def save_network( network, savefile:str ) -> None:
+
+def save_network(network, savefile: str) -> None:
     fh = open(savefile, "wb")
     pickle.dump(network, fh)
 
     fh.close()
 
-def load_network( network, savefile:str ):
+
+def load_network(network, savefile: str):
     fh = open(savefile, "rb")
     network = pickle.load(fh)
     fh.close()
